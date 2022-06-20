9adf3cc7224c1e31c38fa6173007b80cbc4f4924
hlin863 <55798830+hlin863@users.noreply.github.com>
2022-06-20 18:14:44 +0100

    updated flatten_node.cpp

---
+++ flatten_node.cpp
@@ -0,0 +1,154 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+
+using namespace std;
+
+void printList(Node *Node)
+{
+    while (Node != NULL)
+    {
+        printf("%d ", Node->data);
+        Node = Node->bottom;
+    }
+}
+
+Node* flatten (Node* root);
+
+int main(void) {
+
+	int t;
+	cin>>t;
+	while(t--){
+    int n,m,flag=1,flag1=1;
+    struct Node * temp=NULL;
+	struct Node * head=NULL;
+	struct Node * pre=NULL;
+	struct Node * tempB=NULL;
+	struct Node * preB=NULL;
+		cin>>n;
+        int work[n];
+		for(int i=0;i<n;i++)
+            cin>>work[i];
+		for(int i=0;i<n;i++){
+			m=work[i];
+			--m;
+			int data;
+			scanf("%d",&data);
+			temp = new Node(data);
+			temp->next = NULL;
+			temp->bottom = NULL;
+			
+			if(flag){
+				head = temp;
+				pre = temp;
+				flag = 0;
+				flag1 = 1;
+			}
+			else{
+				pre->next = temp;
+				pre = temp;
+				flag1 = 1;
+			}
+			for(int j=0;j<m;j++){
+				
+				int temp_data;
+				scanf("%d",&temp_data);
+				tempB = new Node(temp_data);
+
+				if(flag1){
+					temp->bottom=tempB;
+					preB=tempB;
+					flag1=0;
+				}
+				else{
+					preB->bottom=tempB;
+					preB=tempB;
+				}
+			}
+		}
+		   Node *fun = head;
+		   Node *fun2=head;
+
+            Node* root = flatten(head);
+            printList(root);
+            cout<<endl;
+
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+/* Node structure  used in the program
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+*/
+
+Node* sortedMerge(Node* a, Node* b)  
+{  
+    // code here
+    if(!a)
+    return b;
+    
+    if(!b)
+    return a;
+    
+    if(a->data > b->data)
+    swap(a, b);
+    Node* head=a;
+    
+    while(a != NULL && b != NULL)
+    {
+        Node* previous = NULL;
+        while(a != NULL && a->data <= b->data )
+        {
+            previous = a;
+            a = a->bottom;
+            
+        }
+        previous->bottom = b;
+        swap(a,b);
+    }
+    
+    return head;
+    
+    
+}
+Node *flatten(Node *root)
+{
+   // Your code here
+   Node* head=root;
+   while(root->next!=NULL)
+   {
+       head=sortedMerge(head,root->next);
+       root=root->next;
+       
+   }
+   return head;
+}
+
+
